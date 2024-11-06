6c232e41e76dbc4fae1a915547f09135bc29e1da
Ashil ihsan <ashilihsan@gmail.com>
2024-11-06 19:44:19 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,50 @@
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+
+function randomValueFromArray(array) {
+  const random = Math.floor(Math.random() * array.length);
+  return array[random];
+}
+
+const storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+const insertx = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
+const inserty = ["the soup kitchen", "Disneyland", "the White House"];
+const insertz = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
+
+randomize.addEventListener('click', result);
+
+function result() {
+  let newStory = storyText;
+
+  // Create three new variables xItem, yItem, and zItem
+  const xItem = randomValueFromArray(insertx);
+  const yItem = randomValueFromArray(inserty);
+  const zItem = randomValueFromArray(insertz);
+
+  // Replace placeholders in newStory with values from xItem, yItem, and zItem
+  newStory = newStory.replace(':insertx:', xItem);
+  newStory = newStory.replace(':inserty:', yItem);
+  newStory = newStory.replace(':insertz:', zItem);
+
+  // Replace 'Bob' with custom name if one is provided
+  if (customName.value !== '') {
+    const name = customName.value;
+    newStory = newStory.replace('Bob', name);
+  }
+
+  // Check if the UK radio button is checked, and convert units if so
+  if (document.getElementById("uk").checked) {
+    // Convert 300 pounds to stones and 94 Fahrenheit to centigrade
+    const weight = Math.round(300 * 0.0714286) + ' stone';
+    const temperature = Math.round((94 - 32) * 5 / 9) + ' centigrade';
+
+    // Replace '300 pounds' and '94 fahrenheit' in the story
+    newStory = newStory.replace('300 pounds', weight);
+    newStory = newStory.replace('94 fahrenheit', temperature);
+  }
+
+  // Set the text content of the story paragraph to newStory and make it visible
+  story.textContent = newStory;
+  story.style.visibility = 'visible';
+}
